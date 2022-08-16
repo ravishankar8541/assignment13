3d8f202f5f13b363194c6cdac1196cf2892c172a
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-08-16 09:03:44 +0530

    Add files via upload

--- Q7.c
+++ Q7.c
@@ -1,20 +1,27 @@
-//Write a recursive function to print squares of first N natural numbers
+//Write a recursive function to calculate HCF of two numbers
 #include"stdio.h"
-int PrintSquare(int);
-int PrintSquare(int n)
-{
-    if(n==0)
-        return 1;
-    PrintSquare(n-1);
-    printf("\nSquare of %d is %d",n,n*n);
-
-}
+int HCF(int,int);
 int main()
 {
 
-    int a;
-    printf("Enter a Number :");
-    scanf("%d",&a);
-    PrintSquare(a);
-return 0;
+    int n,n2;
+    printf("Enter first number :");
+    scanf("%d",&n);
+    printf("Enter second number :");
+    scanf("%d",&n2);
+    printf("HCf of %d and %d is %d",n,n2,HCF(n,n2));
+    return 0;
+}
+int HCF(int a,int b)
+{
+   int num1=(a>b)?a:b;
+   int num2=(a<b)?a:b;
+    if(num2==0){
+        return num1;
+        }
+        else
+        {
+
+    return HCF(num2,(num1%num2));
+        }
 }
