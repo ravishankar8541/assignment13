c02f3d68125d1f0aa1e2609305aa60fcd9d469e7
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-08-16 09:05:24 +0530

    Add files via upload

--- Q10.c
+++ Q10.c
@@ -1,20 +1,21 @@
-//. Write a recursive function to print reverse of a given number
+//. Write a program in C to calculate the power of any number using recursion.
 #include"stdio.h"
-int revNum(int);
-int revNum(int n)
+void CalPow(int );
+void CalPow(int n)
 {
     if(n==0)
-        return 1;
-         printf("%d",n%10);
-        revNum(n/10);
-
-}
-    int main()
     {
-      int num;
-      printf("Enter a number :");
-      scanf("%d",&num);
-      revNum(num);
-      return 0;
-
+        return 0;
     }
+    CalPow(n%n);
+    printf("power of %d is %d",n,n*n);
+    return 0;
+}
+int main()
+{
+    int a;
+    printf("Enter a number :");
+    scanf("%d",&a);
+    CalPow(a);
+    return 0;
+}
